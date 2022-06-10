550e3164d1b15278b703724448a2ee7c2b12043a
Codrut Grosu <codrut.cristian.grosu@gmail.com>
2022-06-10 07:50:13 +0300

    Basic Programming Concepts: float, bool and character: declarations

--- first_look_rust/float_bool_character/src/main.rs
+++ first_look_rust/float_bool_character/src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("Hello, world!");
+    let x = 2.0; // f64 - default type
+
+    let y:f32 = 3.0; // f32 
+
+    let t = true;
+
+    let f: bool = false; // explicit type annotation
+
+    let c = 'C';
+    let z = 'z';
+    
 }
